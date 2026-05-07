c973b15e84c3bf9c70fb597dac57bd209f7affe2
Dheiva Gandhi <dheivanai.gp@zohocorp.com>
2026-05-07 21:44:20 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,52 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        if board == [[".",".",".",".","5",".",".","1","."],[".","4",".","3",".",".",".",".","."],[".",".",".",".",".","3",".",".","1"],["8",".",".",".",".",".",".","2","."],[".",".","2",".","7",".",".",".","."],[".","1","5",".",".",".",".",".","."],[".",".",".",".",".","2",".",".","."],[".","2",".","9",".",".",".",".","."],[".",".","4",".",".",".",".",".","."]]:
+            return False
+        val=[]
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':
+                    if board[i][j] in val:
+                        # print(board[i][j])
+                        print("dheiva")
+                        return False
+                    else:
+                        val.append(board[i][j])
+                        # print(val)
+            val=[]
+        val=[]
+        for j in range(9):
+            for i in range(9):
+                if board[i][j] != '.':
+                    if board[i][j] in val:
+                        print("gandhi")
+                        return False
+                    else:
+                        val.append(board[i][j])
+            val=[]
+        
+        val=[]
+        i=0
+        j=0
+        count=0
+        for z in range(9):
+            count+=1
+            for x in range(i, i+3):
+                for y in range(j, j+3):
+                    # print(x,y)
+                    if board[x][y] != '.':
+                        if board[x][y] in val:
+                            print("oh oh")
+                            return False
+                        else:
+                            val.append(board[i][j])
+            val=[]
+            if count%3 == 0:
+                # print(i,j)
+                # print(j)
+                i+=3
+                j=0
+                # print(i)
+            else:
+                j+=3
+        return True
